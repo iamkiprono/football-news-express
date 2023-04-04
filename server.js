8d27ee83e4587b16715d7bece1eddf97b15b69ca
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-04 15:11:27 +0300

    Updates

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+  })
+);
 
-
-const connection = mysql.createConnection(`mysql://wpcemnt3x2xqwddss8wh:pscale_pw_8M3702RzJxk2rj9wnblKEXqbQG16qc2p4Dk1NerzkBJ@aws.connect.psdb.cloud/football?ssl={"rejectUnauthorized":true}`).promise();
+const connection = mysql
+  .createConnection(
+    `mysql://wpcemnt3x2xqwddss8wh:pscale_pw_8M3702RzJxk2rj9wnblKEXqbQG16qc2p4Dk1NerzkBJ@aws.connect.psdb.cloud/football?ssl={"rejectUnauthorized":true}`
+  )
+  .promise();
 app.use(express.json());
 app.get("/", async (req, res) => {
   try {
@@ -21,10 +29,21 @@ app.get("/", async (req, res) => {
 app.post("/news", async (req, res) => {
   const { id, news, details, image, category } = req.body;
   const q = `INSERT INTO Persons(id, news, details, image, category) VALUES('${id}', '${news}', '${details}', '${image}', '${category}')`;
- 
+
   try {
     await connection.query(q);
     res.json(`Added ${news}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/news/:id", async (req, res) => {
+  const { id } = req.params;
+  const q = `DELETE FROM Persons WHERE id = ?`;
+  try {
+    await connection.query(q, [id]);
+    res.json(`${id} deleted`);
   } catch (error) {
     console.log(error);
   }
